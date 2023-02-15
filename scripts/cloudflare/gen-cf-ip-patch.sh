1d83c6acdc3cc6902c8248972c0fbac7b097c0d7
Andrew Powers-Holmes <aholmes@omnom.net>
2023-02-15 23:16:41 +1100

    adjust whitelist patches...

--- scripts/cloudflare/gen-cf-ip-patch.sh
+++ scripts/cloudflare/gen-cf-ip-patch.sh
@@ -17,15 +17,24 @@ trusted_proxy_addrs=(
 
 # generate json array of all cloudflare IPs and rfc1918 addresses
 printf '%s\n' "${trusted_proxy_addrs[@]}" |
-  jq -R "[ inputs | select(length>0) ]" \
+  jq -nR "[ inputs | select(length>0) ]" \
     > "${script_dir}/cloudflare-ips.json"
 
 # generate patch file for traefik helm chart
 patch='[
-  { "op": "add", "path": "/spec/values/ports/web/forwardedHeaders", "value": { "trustedIPs": . } },
-  { "op": "copy", "from": "/spec/values/ports/web/forwardedHeaders", "path": "/spec/values/ports/websecure/forwardedHeaders" }
+  { "op": "add", "path": "/spec/values/ports/websecure/forwardedHeaders/trustedIPs", "value": . },
+  { "op": "copy", "from": "/spec/values/ports/websecure/forwardedHeaders/trustedIPs", "path": "/spec/values/ports/web/forwardedHeaders/trustedIPs" }
 ]'
 
 jq "$patch" "${script_dir}/cloudflare-ips.json" |
   yq -y '.' \
-    > "${script_dir}/../../components/cloudflare/cloudflare-ips.patch.yaml"
+    > "${script_dir}/../../cluster/traefik/patch-ports-trusted-ips.yaml"
+
+# and for traefik middleware
+patch='[
+  { "op": "add", "path": "/spec/ipWhiteList/ipStrategy/excludedIPs", "value": . }
+]'
+
+jq "$patch" "${script_dir}/cloudflare-ips.json" |
+  yq -y '.' \
+    > "${script_dir}/../../cluster/traefik/patch-ipwhitelist-excluded-ips.yaml"
